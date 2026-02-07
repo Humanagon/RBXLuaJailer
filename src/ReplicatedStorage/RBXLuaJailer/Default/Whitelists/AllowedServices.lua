ee652ec8eae3136428ed746e9587a9c35716f7e0
Humanagon <57272456+Humanagon@users.noreply.github.com>
2026-02-07 09:24:51 -0600

    Add files via upload

---
+++ src/ReplicatedStorage/RBXLuaJailer/Default/Whitelists/AllowedServices.lua
@@ -0,0 +1,15 @@
+--Whitelist for which game services can be accessed with the "game" variable.
+
+return {
+	"Debris",
+	"Players",
+	"Lighting",
+	"ReplicatedFirst",
+	"ReplicatedStorage",
+	"RunService",
+	"ServerScriptService",
+	"ServerStorage",
+	"StarterPack",
+	"Teams",
+	"Workspace"
+}
